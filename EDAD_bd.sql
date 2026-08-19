98f5f361de9eb0da4625be19106547ecd1b7592d
Jorgejbf0 <jorgitojbueno@gmail.com>
2026-08-18 22:43:03 -0300

    banco de dados completo para teste 2

--- EDAD_bd.sql
+++ EDAD_bd.sql
@@ -16,6 +16,7 @@ USE EDAD;
 CREATE TABLE userFac(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
     RA VARCHAR(255) NOT NULL UNIQUE,
+     nome VARCHAR(255) NOT NULL,
     PASS VARCHAR(250) NOT NULL,
     curso VARCHAR(255) NOT NULL,
     turno ENUM('matutino', 'noturno') NOT NULL,
@@ -31,6 +32,7 @@ CREATE TABLE userFac(
 CREATE TABLE userTec(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
     RA VARCHAR(255) NOT NULL UNIQUE,
+    nome VARCHAR(255) NOT NULL,
     PASS VARCHAR(250) NOT NULL,
     curso VARCHAR(255) NOT NULL,
     turno ENUM('matutino', 'noturno') NOT NULL,
@@ -62,6 +64,7 @@ CREATE TABLE arquivos(
     estado ENUM('recebido','negado','aprovado') NOT NULL DEFAULT 'recebido',
     idtec INT NOT NULL,
     idfac INT NOT NULL,
+    arquivo VARCHAR(255) unique,
     CONSTRAINT fk_userTec
     FOREIGN KEY (idtec)
     REFERENCES userTec(id),
@@ -83,10 +86,42 @@ CREATE TABLE resp(
     REFERENCES userADM(id)
 );
 
+CREATE TABLE audit(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tabela VARCHAR(250) NOT NULL,
+    operation_type ENUM('insert','update','delete') NOT NULL,
+    old_data JSON,
+    new_data JSON NOT NULL,
+    issuer_id INT NOT NULL,
+    operation_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_users_audit
+    FOREIGN KEY (issuer_id)
+    REFERENCES userTec(id)
+);
+
 INSERT INTO usertec(RA,PASS,curso,turno) VALUES ("Jorgejbf",MD5("123"),"informatica","noturno");
 INSERT INTO arquivos(descricao,idtec,idfac) VALUES ('Em analise','1','1');
 
 SELECT ut.RA , a.id from usertec ut LEFT JOIN arquivos a ON ut.id = a.idtec;
+
+
+
+DELIMITER $$
+    CREATE PROCEDURE add_arquivos_audit(
+        IN a_descricao VARCHAR(255),
+        IN a_estado ENUM,
+        IN r_resposta text
+    )
+BEGIN
+     DECLARE id INT;
+    INSERT INTO userTec(RA,PASS,curso,turno,situacao) VALUES (t_RA,t_PASS,t_curso,t_turno,t_situacao);
+    -- SET t_id = (SELECT id FROM userTec WHERE RA = t_RA);
+    SET t_RA = LAST_INSERT_ID();
+    INSERT INTO arquivos(idtec) VALUES (user_id,p_ammount);
+
+END $$
+
+
 
 
 -- Base de Exemplo
@@ -184,32 +219,3 @@ Ultima atualização: 13-08-2026/19:47
 
 -- SELECT u.username , p.ammount from users u LEFT JOIN points p ON u.id = p.user WHERE u.username = "KevinAzedo";
 
-
-CREATE DATABASE EDAD;
-USE EDAD;
-
-CREATE TABLE userTec(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(250) NOT NULL,
-    email VARCHAR(250) NOT NULL,
-    PASS VARCHAR(250) NOT NULL,
-    Curso VARCHAR(255) NOT NULL,
-    Turno int NOT NULL,
-    estado VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE arquivos(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    Readname VARCHAR(255) not NULL,
-    email VARCHAR(255) NOT NULL,
-    PASS VARCHAR(255) NOT NULL,
-    estado VARCHAR(255) NOT NULL,
-    resposta VARCHAR(255) NOT NULL
-);
-
-
-INSERT INTO user(username,email,PASS) VALUES ("Jorgejbf","jorgejbf@gmail.com",MD5("123"));
-INSERT INTO arquivos(username) VALUES (1);
-
-SELECT u.username , a.id from usere u LEFT JOIN points p ON u.id = a.username;
